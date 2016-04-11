d927774d11e659a758ff979cdf12abcb0b3bfbe0
HaroldMills <harold.mills@gmail.com>
2016-04-11 14:34:13 -0400

    Added a unit test.

---
+++ harold_packaging_example/util/tests/test_utils.py
@@ -0,0 +1,16 @@
+import unittest
+
+from .. import utils
+
+
+class UtilsTests(unittest.TestCase):
+    
+    def test_translate(self):
+        
+        cases = [
+            ('hallo', 'hello'),
+            ('bobo', 'bobo')
+        ]
+        
+        for s, expected in cases:
+            self.assertEqual(utils.translate(s), expected)
