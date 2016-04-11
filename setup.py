7caabb865dcc97e53e9a48691c1560d157a2b7a2
HaroldMills <harold.mills@gmail.com>
2016-04-11 13:08:33 -0400

    Updated setup script.

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from setuptools import setup
 
 setup(
     name='harold_packaging_example',
-    version='0.1.0',
+    version='0.1.4',
     description='Simple example of Python packaging.',
     url='https://github.com/HaroldMills/Python-Packaging-Example',
     author='Harold Mills',
     author_email='harold.mills@gmail.com',
     license='MIT',
     packages=['harold_packaging_example', 'harold_packaging_example.util'],
+    install_requires=['pyyaml'],
     entry_points={
         'console_scripts': [
             'test_script=harold_packaging_example.module:main'
