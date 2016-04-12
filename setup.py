2a9b429430775af61018a79eacd935437157f28b
HaroldMills <harold.mills@gmail.com>
2016-04-12 09:13:48 -0400

    Modified MANIFEST.in.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='harold_packaging_example',
-    version='0.1.5',
+    version='0.1.6',
     description='Simple example of Python packaging.',
     url='https://github.com/HaroldMills/Python-Packaging-Example',
     author='Harold Mills',
