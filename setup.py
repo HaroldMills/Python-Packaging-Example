db6b27aa9e0cfdd67afcf1e39a50ddda05f0f3f9
HaroldMills <harold.mills@gmail.com>
2016-04-12 10:38:30 -0400

    Tweaked packaging files.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='harold_packaging_example',
-    version='0.1.7',
+    version='0.1.8',
     description='Simple example of Python packaging.',
     url='https://github.com/HaroldMills/Python-Packaging-Example',
     author='Harold Mills',
